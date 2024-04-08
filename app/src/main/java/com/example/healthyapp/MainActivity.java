80192a98c253f6728bb6ee313786373f8ae92d49
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-04-08 20:38:07 +0700

    a

--- app/src/main/java/com/example/healthyapp/MainActivity.java
+++ app/src/main/java/com/example/healthyapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.healthyapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.example.healthyapp.DBConnetion.FirebaseDBConnection;
