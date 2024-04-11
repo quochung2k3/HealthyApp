0f4a94f0f31389b0c8c563c380c2626eaea1cd1b
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-04-11 13:12:09 +0700

    Update Sign Up

--- app/src/main/java/com/example/healthyapp/SignUpActivity.java
+++ app/src/main/java/com/example/healthyapp/SignUpActivity.java
@@ -1,6 +1,5 @@
 package com.example.healthyapp;
 
-import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -9,25 +8,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.healthyapp.models.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -82,9 +77,11 @@ public class SignUpActivity extends AppCompatActivity {
                             userID = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
                             System.out.println("Test " + userID);
                             DocumentReference documentReference = firestore.collection("users").document(userID);
-                            Map<String, String> user = new HashMap<>();
-                            user.put("fName", firstName);
-                            user.put("lName", lastName);
+                            UserModel user = new UserModel();
+                            user.setFirst_name(firstName);
+                            user.setLast_name(lastName);
+                            user.setEmail(email);
+                            user.setPassword(password);
                             documentReference.set(user)
                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
