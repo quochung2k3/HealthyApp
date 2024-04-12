a65dbf6ff85e320d1085dca086f3c80bad524f13
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-04-12 15:39:52 +0700

    SendMess

--- app/src/main/java/com/example/healthyapp/ChatActivity.java
+++ app/src/main/java/com/example/healthyapp/ChatActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,7 +18,10 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.healthyapp.adapter.MessAdapter;
 import com.example.healthyapp.databinding.ActivityMainBinding;
 import com.example.healthyapp.fragments.HomeFragment;
 import com.example.healthyapp.fragments.MenuFragment;
@@ -26,24 +31,43 @@ import com.example.healthyapp.fragments.UpdatePassFragment;
 import com.example.healthyapp.models.MessageModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     TextView txtUsername;
     EditText edtMess;
     ImageView imgBack, imgSendMess;
+    MessAdapter messAdapter;
+    List<MessageModel> listMessage;
+    RecyclerView recyclerView;
+    Intent intent;
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://healthyapp-bfba9-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    DatabaseReference databaseReference;
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         Mapping();
-        Intent intent = getIntent();
+        intent = getIntent();
         if (intent != null) {
             String userName = intent.getStringExtra("userName");
             txtUsername.setText(userName);
         }
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        readMess(firebaseUser.getUid(), intent.getStringExtra("id"));
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,18 +97,21 @@ public class ChatActivity extends AppCompatActivity {
         imgSendMess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-                MessageModel message = new MessageModel();
-                message.setContent(edtMess.getText().toString());
-                assert firebaseUser != null;
-                message.setSender_id(firebaseUser.getUid());
-                assert intent != null;
-                message.setReceiver_id(intent.getStringExtra("id"));
-                message.setIs_deleted(false);
-                FirebaseDatabase database = FirebaseDatabase.getInstance("https://healthyapp-bfba9-default-rtdb.asia-southeast1.firebasedatabase.app/");
-                DatabaseReference databaseReference = database.getReference();
-                databaseReference.child("Message").push().setValue(message);
-                edtMess.setText("");
+                if(!edtMess.getText().toString().isEmpty()) {
+                    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                    MessageModel message = new MessageModel();
+                    message.setContent(edtMess.getText().toString());
+                    assert firebaseUser != null;
+                    message.setSender_id(firebaseUser.getUid());
+                    assert intent != null;
+                    message.setReceiver_id(intent.getStringExtra("id"));
+                    message.setIs_deleted(false);
+                    database.getReference().child("Message").push().setValue(message);
+                    edtMess.setText("");
+                }
+                else {
+                    Toast.makeText(ChatActivity.this, "Không thể gởi tin nhắn với nội dung rỗng", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -101,5 +128,31 @@ public class ChatActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    public void readMess(String myId, String userId) {
+        listMessage = new ArrayList<>();
+        databaseReference = database.getReference("Message");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listMessage.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    MessageModel message = dataSnapshot.getValue(MessageModel.class);
+                    assert message != null;
+                    if((message.getReceiver_id().equals(myId) && message.getSender_id().equals(userId))
+                            || (message.getReceiver_id().equals(userId) && message.getSender_id().equals(myId))) {
+                        listMessage.add(message);
+                    }
+                    messAdapter = new MessAdapter(ChatActivity.this, listMessage);
+                    recyclerView.setAdapter(messAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
